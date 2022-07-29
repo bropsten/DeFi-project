9c3233ccfcd86ba9157a2e3f4c82c76c1b7e8650
Akumaw <dev@akumaw.com>
2022-07-29 10:04:34 +0200

    feature: add front un logged user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
 import PreHeader from './components/PreHeader';
 import "./App.css";
+import Hero from './components/Hero';
+import Team from './components/Team';
+import Partners from './components/Partners';
+import Contacts from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
+
+    <>
       <PreHeader />
-    </div>
-  );
+      <Hero />
+      <Team />
+      <Partners />
+      <Contacts />
+      <Footer />
+
+    </>
+
+  )
+
 }
 
 export default App;
