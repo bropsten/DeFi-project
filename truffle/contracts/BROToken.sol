c8ae4dbd3a2fb6e2214a027740f608e56f4099a2
VicLam <tanacetifolia@gmail.com>
2022-08-01 13:56:40 +0200

    rename reward token & natspec for staking

--- truffle/contracts/BROToken.sol
+++ truffle/contracts/BROToken.sol
@@ -3,10 +3,12 @@ pragma solidity ^0.8.7;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-
-contract RewardToken is ERC20 {
-    constructor() ERC20("Reward Token", "RT") {
+contract BROToken is ERC20 {
+    constructor() ERC20("Bropsten Token", "BRO") {
         _mint(msg.sender, 1000000 * 10**18);
     }
 
+    function faucet(address _recipient) external {
+        _mint(_recipient, 1000 * 10 ** 18);
+    }
 }
