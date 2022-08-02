3fe02b192449cc8848014c4f3b5909c7db7b65a5
Akumaw <dev@akumaw.com>
2022-08-02 11:47:19 +0200

    feat add mint part + locking time tickbox

--- client/src/components/Staking.js
+++ client/src/components/Staking.js
@@ -2,9 +2,7 @@ import { CheckCircleIcon } from "@heroicons/react/solid";
 import React, { useState} from "react";
 
 
-export default function Staking({contract}) {
-
-  console.log("staking", contract);
+export default function Staking({contract, account}) {
 
 
     const [amountToStake, setAmountToStake] = useState(0);
@@ -13,10 +11,23 @@ export default function Staking({contract}) {
 
       const element = document.getElementById("stake");
       const amountToStake = element.value;
-      console.log(amountToStake);
+      let transactionAddStaking;
 
-      setAmountToStake(amountToStake);
-   
+      console.log(contract);
+
+      const checkBox = document.getElementById("lockedTime");
+      const checkBoxTickOrNot = checkBox.checked;
+      console.log(checkBoxTickOrNot);
+
+      try {
+        transactionAddStaking = await contract.methods.stake(amountToStake, checkBoxTickOrNot).send({from: account});
+        console.log(transactionAddStaking);
+        
+        setAmountToStake(amountToStake);
+      }catch(err){
+        console.log(err);
+      }
+
       element.value = "";
   }
   
@@ -40,6 +51,40 @@ export default function Staking({contract}) {
         </div>
       </div>
 
+      <div className="bg-white shadow sm:rounded-lg mt-10">
+        <div className="px-4 py-5 sm:p-6">
+          <h3 className="text-lg leading-6 font-medium text-gray-900">
+            Please mint some token
+          </h3>
+          <div className="mt-2 max-w-xl text-sm text-gray-500">
+            <p>
+              By clicking this button you will receive 1000 BRO Tokens
+            </p>
+          </div>
+          <form className="mt-5 sm:flex sm:items-center">
+            <div className="w-full sm:max-w-xs">
+              <label htmlFor="mint" className="sr-only">
+                Mint
+              </label>
+              <input
+                type="text"
+                name="price"
+                id="mint"
+                className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+                placeholder="0.00"
+                aria-describedby="price-currency"
+              />
+            </div>
+            <button
+              type="submit"
+              className="mt-3 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+            >
+              Mint
+            </button>
+          </form>
+        </div>
+      </div>
+
       <div className="rounded-md bg-green-50 p-4 my-10">
         <div className="flex">
           <div className="flex-shrink-0">
@@ -49,13 +94,9 @@ export default function Staking({contract}) {
             />
           </div>
           <div className="ml-3">
-            <h3 className="text-sm font-medium text-green-800">
-              Claim reward 
-            </h3>
+            <h3 className="text-sm font-medium text-green-800">Claim reward</h3>
             <div className="mt-2 text-sm text-green-700">
-              <p>
-                You have 100$ ready to claim
-              </p>
+              <p>You have 100$ ready to claim</p>
             </div>
             <div className="mt-4">
               <div className="-mx-2 -my-1.5 flex">
@@ -103,12 +144,36 @@ export default function Staking({contract}) {
             </div>
             <button
               type="submit"
-              onClick = {addStaking}
+              onClick={addStaking}
               className="mt-3 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
             >
               STAKE
             </button>
           </div>
+
+          <fieldset className="space-y-5">
+            <legend className="sr-only">Notifications</legend>
+            <div className="relative flex items-start">
+              <div className="flex items-center h-5">
+                <input
+                  id="lockedTime"
+                  aria-describedby="comments-description"
+                  name="lockedTime"
+                  type="checkbox"
+                  className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                />
+              </div>
+              <div className="ml-3 text-sm">
+                <label htmlFor="comments" className="font-medium text-gray-700">
+                  Locking time
+                </label>
+                <p id="comments-description" className="text-gray-500">
+                  If you tick the case, you will lock your token for one month<br></br>your APY will be x1000000 so that's great
+                </p>
+              </div>
+            </div>
+          </fieldset>
+
         </div>
       </div>
 
@@ -145,6 +210,7 @@ export default function Staking({contract}) {
           </form>
         </div>
       </div>
+      
     </>
   );
 }
