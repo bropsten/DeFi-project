e6ac305f34ed0e840f449c12ac16d568d5eace93
Akumaw <dev@akumaw.com>
2022-08-03 10:28:10 +0200

    finish front Dapp

--- client/src/components/Staking.js
+++ client/src/components/Staking.js
@@ -1,16 +1,17 @@
 import { CheckCircleIcon } from "@heroicons/react/solid";
-import BigNumber from "bignumber.js"
 import React, { useEffect, useState} from "react";
 import useWeb3 from "../hooks/useWeb3";
 
 export default function Staking({contract, account}) {
 
   const{web3} = useWeb3();
-  const [contractBalance, setContractBalance] = useState(0);
   const [totalSupply, setTotalSupply] = useState(0);
-  const [userTokenStaked, setUserTokenStaked] = useState(0);
   const [userStakingReward, setUserStakingReward] = useState(0);
   const [broTokenAddress, setBroTokenAddress] = useState('');
+  const [userStakedBalance, setUserStakedBalance] = useState(0);
+  const [userLockedBalance, setuserLockedBalance] = useState(0);
+  const [userRewardBalance, setUserRewardBalance] = useState(0);
+  const [lockedDeadline, setLockedDeadline] = useState(0);
 
   useEffect(() => {
     if( ! contract ){
@@ -18,96 +19,107 @@ export default function Staking({contract, account}) {
     }
 
     setBroTokenAddress(contract.BROToken._address);
+    updateInfos();
 
-    async function getUserTokenStaked() {
-      let userTokenStaked = await contract.staking.methods.balances(account).call();
-      console.log(userTokenStaked);
-      userTokenStaked = web3.utils.fromWei(userTokenStaked, 'ether');
-      
-      // we get all event
-      const pastTokenStake = await contract.staking.getPastEvents('Stake', { fromBlock: 0, toBlock: 'latest' });
-      console.log(pastTokenStake);
-
-      // we get all the ammount staked by the user
-      const pastTokenStakeBalance = pastTokenStake.map(event => {
-        return event.returnValues.amount;
-      });
-      console.log(pastTokenStakeBalance);
-
-      let totalBalanceUserStaked = 0;
-
-      pastTokenStakeBalance.forEach((value, i) => {
-        totalBalanceUserStaked += parseFloat(value);
-        
-      });
-
-      totalBalanceUserStaked = new BigNumber(totalBalanceUserStaked).toFixed(0);
-      totalBalanceUserStaked = web3.utils.fromWei(totalBalanceUserStaked, 'ether');
-      setUserTokenStaked(totalBalanceUserStaked);
-
-    }
-
-    async function getYourOwnBalance() {
-      let balance = await contract.BROToken.methods.balanceOf(account).call();
-      balance =  web3.utils.fromWei(balance, 'ether');
-      console.log(balance);
-      setContractBalance(balance);
-    }
-
-    async function getTotalSupply() {
-      let totalSupply = await contract.staking.methods.totalSupply().call();
-      totalSupply = web3.utils.fromWei(totalSupply, 'ether');
-      console.log(totalSupply);
-      setTotalSupply(totalSupply);
-    }
-
-
-    async function getUserStakingReward() {
-      let userStakingReward = await contract.staking.methods.rewardsBalance(account).call();
-      userStakingReward = web3.utils.fromWei(userStakingReward, 'ether');
-      console.log(userStakingReward);
-      setUserStakingReward(userStakingReward);
-    }
-
-    getUserStakingReward();
-    getUserTokenStaked();
-    getTotalSupply()
-    getYourOwnBalance()
 }, [contract, account]);
 
+  async function getUserStakedBalance() {
+    const stakedBalance = await contract.staking.methods.getStakedBalance(account).call();
+    console.log("staked balance (wei)", stakedBalance);
+    setUserStakedBalance(convertFromWei(stakedBalance));
+  }
 
-  async function addStaking(){
-    const element = document.getElementById("stake");
-    const amountToStake = element.value;
-    let transactionAddStaking;
+  async function getUserLockedStakedBalance() {
+    const lockedBalance = await contract.staking.methods.lockedBalances(account).call();
+    console.log("locked balance (wei)", lockedBalance.balance);
+    setuserLockedBalance(convertFromWei(lockedBalance.balance));
+  }
 
-    const checkBox = document.getElementById("lockedTime");
-    const checkBoxTickOrNot = checkBox.checked.toString();
-    console.log(checkBoxTickOrNot);
+  async function getUserRewardBalance() {
+    const rewardBalance = await contract.BROToken.methods.balanceOf(account).call();
+    console.log("reward balance (wei)", rewardBalance);
+    setUserRewardBalance(convertFromWei(rewardBalance));
+  }
 
-    try {
-      let approve = await contract.BROToken.methods.approve(contract.staking._address, amountToStake).send({ from: account });
-      console.log(approve)
-      transactionAddStaking = await contract.staking.methods.stake(amountToStake, checkBoxTickOrNot).send({from: account});
-      console.log(transactionAddStaking);
-      
-    }catch(err){
-      console.log(err);
-    }
+  async function getUnlockDeadline() {
+    const lockedBalance = await contract.staking.methods.lockedBalances(account).call();
+    console.log("locked deadline (timestamp)", lockedBalance.deadline);
+    const dateObject = new Date(lockedBalance.deadline * 1000);
+    const date = dateObject.toLocaleString();
+    setLockedDeadline(date);
+  }
 
+  async function getTotalSupply() {
+    let totalSupply = await contract.staking.methods.totalSupply().call();
+    console.log("totalSupply (wei)", totalSupply);
+    setTotalSupply(convertFromWei(totalSupply));
+  }
+
+  function updateInfos() {
+    getUserStakedBalance();
+    getUserLockedStakedBalance();
+    getUnlockDeadline();
+    getUserRewardBalance();
+    getTotalSupply();
+  }
+  
+  function convertFromWei(value) {
+    return web3.utils.fromWei(value, "ether");
+  }
+
+  function convertToWei(value) {
+    return web3.utils.toWei(value, "ether");
+  }
+
+  function getInput(id) {
+    const element = document.getElementById(id);
+    const value = element.value;
     element.value = "";
+    return value;
+  }
+
+  async function addStaking() {
+    const value = getInput("stake");
+
+    if (value != "") {
+      const amountToStake = convertToWei(value);
+      const checkBox = document.getElementById("lockedTime");
+      const checkBoxTickOrNot = checkBox.checked;
+      
+      try {
+        await contract.BROToken.methods.approve(contract.staking._address, amountToStake).send({ from: account });
+        await contract.staking.methods.stake(amountToStake, checkBoxTickOrNot).send({ from: account });
+        updateInfos();
+      } catch (err) {
+        console.error("stake amount", err);
+      }
+    }
+  }
+
+  async function withdraw() {
+    const value = getInput("withdraw");
+    
+    if (value != "") {
+      const amountToWithdraw = convertToWei(value);
+      
+      try {
+        // await contract.BROToken.methods.approve(contract.staking._address, amountToWithdraw).send({ from: account });
+        await contract.staking.methods.withdraw(amountToWithdraw).send({ from: account });
+        updateInfos();
+      } catch (err) {
+        console.error("withdraw amount", err);
+      }
+    }
   }
 
   async function mint() {
     try {
-      let transaction = await contract.BROToken.methods.faucet(account).send({from: account});
-      console.log(transaction);
-    }catch(err){
-      console.log(err);
+      await contract.BROToken.methods.faucet(account).send({from: account});
+      updateInfos();
+    } catch (err) {
+      console.error("mint", err);
     }
   }
-
-  
 
   return (
     <>
@@ -120,11 +132,12 @@ export default function Staking({contract, account}) {
         <div className="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
           <div>
             <ul className="mt-1 max-w-2xl text-blue-sm text-gray-500">
-              <li>Your token amount staked : {userTokenStaked}</li>
-              <li>Your BROtoken wallet balance : {contractBalance} </li>
+              <li>Your tokens amount staked : {userStakedBalance}</li>
+              <li>Your locked tokens amount staked : {userLockedBalance}</li>
+              <li>Your tokens will be unlock the {lockedDeadline}</li>
+              <li>Your BROtoken wallet balance : {userRewardBalance} </li>
               <li>Total supply : {totalSupply}</li>
               <li>Your staking reward : {userStakingReward} </li>
-              
             </ul>
           </div>
         </div>
@@ -256,15 +269,15 @@ export default function Staking({contract, account}) {
               Change the email address you want associated with your account.
             </p>
           </div>
-          <form className="mt-5 sm:flex sm:items-center">
-            <div className="w-full sm:max-w-xs">
-              <label htmlFor="stake" className="sr-only">
-                Stake
+          <div className="mt-5 sm:flex sm:items-center">
+          <div className="w-full sm:max-w-xs">
+              <label htmlFor="withdraw" className="sr-only">
+                Withdraw
               </label>
               <input
                 type="text"
-                name="price"
-                id="price"
+                name="withdraw"
+                id="withdraw"
                 className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
                 placeholder="0.00"
                 aria-describedby="price-currency"
@@ -272,14 +285,15 @@ export default function Staking({contract, account}) {
             </div>
             <button
               type="submit"
+              onClick={withdraw}
               className="mt-3 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
             >
               WITHDRAW
             </button>
-          </form>
+          </div>
+            
         </div>
       </div>
-      
     </>
   );
 }
