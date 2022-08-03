4de8ca42bd54376116ae847913187de6ec85bcb2
Akumaw <dev@akumaw.com>
2022-08-03 15:52:22 +0200

    finish front UI

--- client/src/components/PreHeader.js
+++ client/src/components/PreHeader.js
@@ -26,7 +26,7 @@ export default function PreHeader() {
           
               className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
             >
-             Current APY : 20%
+             Reward per token : 0.01 ETH
             </button>
           </div>
          
